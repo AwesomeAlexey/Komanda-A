72c60ebad183b86ecb0076ba9681cabe3667c727
Maksim Surov <surov.m.o@gmail.com>
2023-05-02 19:34:34 +0300

    control system

--- src/control_system/src/control_system.cpp
+++ src/control_system/src/control_system.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 
-int launch(Json::Value const& jscfg)
+int launch(Json::Value const& jscfg, Json::Value const& ctrlcfg)
 {
     Butterfly bfly;
     bfly.init(jscfg);
@@ -18,21 +18,17 @@ int launch(Json::Value const& jscfg)
     auto stop_handler = [&stop, &bfly]() { stop = true; bfly.stop(); };
     SysSignals::instance().set_sigint_handler(stop_handler);
     SysSignals::instance().set_sigterm_handler(stop_handler);
+    SlidingController ctrl;
+    ctrl.load(ctrlcfg);
 
-    // overeturn
-    auto f = [](BflySignals& signals) {
+
+    auto f = [&ctrl](BflySignals& signals) {
         if (signals.t < 0.1)
             return true;
-
         if (!signals.ball_found)
             return false;
-
-        // signals.torque = get_torque(signals.theta, signals.phi, signals.dtheta, signals.dphi);
-        // signals.torque = clamp(signals.torque, -0.1, 0.1);
-    
-        info_msg("t=", signals.t, ",torque=", signals.torque, ",theta=", signals.theta, ",phi=", signals.phi, 
-             ",dtheta=", signals.dtheta, ",dphi=", signals.dphi, ",x=", signals.x, ",y=", signals.y);
-
+        double u = ctrl.process(signals.theta, signals.phi, signals.dtheta, signals.dphi);
+        signals.torque = clamp(u, -0.1, 0.1);
         return true;
     };
 
@@ -43,7 +39,8 @@ int launch(Json::Value const& jscfg)
 int main(int argc, char const* argv[])
 {
     Arguments args({
-        Argument("-c", "config", "path to json config file", "", ArgumentsCount::One)
+        Argument("-c", "config", "path to the butterfly robot config file", "", ArgumentsCount::One),
+        Argument("-f", "controller", "path to controller config file", "", ArgumentsCount::One)
     });
 
     int status = 0;
@@ -51,9 +48,10 @@ int main(int argc, char const* argv[])
     try
     {
         auto&& m = args.parse(argc, argv);
-        Json::Value const& cfg = json_load(m["config"]);
-        traces::init(json_get(cfg, "traces"));
-        launch(cfg);
+        Json::Value const& butcfg = json_load(m["config"]);
+        traces::init(json_get(butcfg, "traces"));
+        Json::Value const& ctrlcfg = json_load(m["controller"]);
+        launch(butcfg, ctrlcfg);
     }
     catch (exception const& e)
     {
